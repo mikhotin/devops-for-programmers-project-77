e5d89e508b096479b34aca3b4203fffaf63266fd
mikhotin <ad.mihotin@gmail.com>
2024-12-23 19:45:48 +0300

    wip

--- terraform/generate_inventory.tf
+++ terraform/generate_inventory.tf
@@ -13,3 +13,11 @@ EOT
     yandex_compute_instance.vm-2
   ]
 }
+
+output "vm-1_external_ip" {
+  value = yandex_compute_instance.vm-1.network_interface[0].nat_ip_address
+}
+
+output "vm-2_external_ip" {
+  value = yandex_compute_instance.vm-2.network_interface[0].nat_ip_address
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ data "yandex_compute_image" "img" {
 }
 
 resource "yandex_compute_instance" "vm-1" {
-  name        = "test"
+  name        = "vm-1"
   platform_id = "standard-v1"
   zone        = "ru-central1-a"
 
@@ -64,19 +64,3 @@ resource "yandex_vpc_address" "addr" {
 resource "yandex_alb_http_router" "router" {
   name = "my-http-router"
 }
-
-output "vm-1_external_ip" {
-  value = yandex_compute_instance.vm-1.network_interface[0].nat_ip_address
-}
-
-output "vm-2_external_ip" {
-  value = yandex_compute_instance.vm-2.network_interface[0].nat_ip_address
-}
-
-# resource "yandex_organizationmanager_user_ssh_key" "ssh_key" {
-#   organization_id = "some_organization_id"
-#   subject_id      = "some_subject_id"
-#   data            = var.ssh
-# }
-
-
